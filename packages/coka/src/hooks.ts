36567cbf7b542a2c0ac2736e2c389ed580f71baa
沈赟杰 <shenyunjie@znrank.com>
2022-06-01 22:37:02 +0800

    update

--- packages/coka/src/hooks.ts
+++ packages/coka/src/hooks.ts
@@ -1,8 +1,5 @@
 import { useContext } from 'react';
 import { RequestContext, RuntimeModeContext } from './coka';
-import { TUseData } from './types';
-
-const cache: Record<string, TUseData> = {};
 
 export function useRuntimeMode() {
   return useContext(RuntimeModeContext);
@@ -30,47 +27,4 @@ export function useHash<T>(format?: (v: string) => T) {
   const request = useContext(RequestContext);
   const value = request.hash;
   return format ? format(value) : value;
-}
-
-export function useData<T = any>(
-  key: string, 
-  fetcher: (key?: string) => Promise<T>, 
-  deps: any[] = []
-) {
-  function mutate() {
-    cache[key].isValidating = true;
-    return fetcher(key).then(
-      (r) => {
-        cache[key].isValidating = false;
-        cache[key].data = r;
-        return r;
-      },
-      (err) => {
-        cache[key].isValidating = false;
-        console.error(err);
-      }
-    );
-  }
-
-  const createFetcher = () => () => {
-    const { data, isValidating, promise } = cache[key];
-    if (data !== undefined && !isValidating) {
-      return data;
-    }
-    if (!promise) {
-      cache[key].promise = mutate();
-    }
-    throw cache[key].promise;
-  };
-
-  if (!cache[key]) {
-    cache[key] = {
-      data: undefined,
-      promise: null,
-      isValidating: false,
-    };
-    cache[key].fn = createFetcher();
-  }
-
-  return cache[key].fn();
 }
