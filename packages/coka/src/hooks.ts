075d981a46877bf5072a81d91e78b795cb9cc062
沈赟杰 <shenyunjie@znrank.com>
2022-06-28 18:11:20 +0800

    update

--- packages/coka/src/hooks.ts
+++ packages/coka/src/hooks.ts
@@ -7,24 +7,28 @@ export function useRuntimeMode() {
 
 export function useQuery<T>(key: string, format?: (v: string | string[]) => T) {
   const request = useContext(RequestContext);
+  if (!request) return null;
   const value = request.query[key];
   return format ? format(value) : value;
 }
 
 export function useParam<T>(key: string, format?: (v: string) => T) {
   const request = useContext(RequestContext);
+  if (!request) return null;
   const value = request.params[key];
   return format ? format(value) : value;
 }
 
 export function usePath<T>(format?: (v: string) => T) {
   const request = useContext(RequestContext);
+  if (!request) return null;
   const value = request.pathname;
   return format ? format(value) : value;
 }
 
 export function useHash<T>(format?: (v: string) => T) {
   const request = useContext(RequestContext);
+  if (!request) return null;
   const value = request.hash;
   return format ? format(value) : value;
 }
